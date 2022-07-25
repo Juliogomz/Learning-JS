883790dee792cec06097b498a726decfa58cc880
Julio Gomes <106027379+Juliogomz@users.noreply.github.com>
2022-07-24 23:13:23 -0300

    JS-Update

---
+++ Atividades Básicas-JS/Atividade Básica 02/Atividade Básica 02.js
@@ -0,0 +1,11 @@
+//Troque os valores armazenados nas variáveis A = 20 e B=50.  
+
+let A = 20
+let B = 50
+let temp
+
+temp = A
+A = B
+B = temp
+
+console.log("A é igual a: " + A + " e B é igual a: " + B)
