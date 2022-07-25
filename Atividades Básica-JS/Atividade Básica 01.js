7f18b9fc7ea45e1bcc295aee32e433dbf107b0db
Julio Gomes <106027379+Juliogomz@users.noreply.github.com>
2022-07-24 23:11:12 -0300

    JS-Update

---
+++ Atividades Básica-JS/Atividade Básica 01.js
@@ -0,0 +1,8 @@
+/*Calcule a área de um círculo de raio de 10 metros. Fórmula π * raio * raio. 
+Sendo π = 3.141592.*/
+
+const PI = 3.141592
+let raio = 10
+let areacir = 3.141592 * 10 * 10
+
+console.log("A área do círculo é: " + areacirc + " m2.")
