cd18db72946d2f207c2ae80deae820c51c242d9c
Julio Gomes <106027379+Juliogomz@users.noreply.github.com>
2022-08-01 19:04:48 -0300

    JS-Update

---
+++ Aprendendo função/Armazenando uma função em uma variavel/Função em uma variável.js
@@ -0,0 +1,11 @@
+//Função em uma variável
+
+const soma =  function (a,b){
+    console.log(a + b)
+}
+
+soma(7,3)
+soma(9,5)
+soma(22,88)
+soma(43,61)
+soma(8,1)
---
+++ Aprendendo função/Função de soma/Função de soma.js
@@ -0,0 +1,11 @@
+//Exemplo de soma com função utilizando valores inteiros aleatórios
+
+function realizarsoma(a, b) {
+console.log(a + b)
+}
+
+realizarsoma(5, 9)
+realizarsoma(3, 7)
+realizarsoma(20, 35)
+realizarsoma(33, 17)
+realizarsoma(7, 15)
