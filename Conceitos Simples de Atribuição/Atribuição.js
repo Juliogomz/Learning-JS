1b5222deea3934003e43373edbe2e7b384058688
Julio Gomes <106027379+Juliogomz@users.noreply.github.com>
2022-08-06 21:52:49 -0300

    JS-Update

---
+++ Conceitos Simples de Atribuição/Atribuição.js
@@ -0,0 +1,17 @@
+const a = 7
+let b = 3
+
+b += a        // igual a b = b + a
+console.log(b)
+
+b -= a        // igual a b = b - a
+console.log(b)
+
+b *= a        // igual a b = b * a
+console.log(b)
+
+b /= a        // igual a b = b / a
+console.log(b)
+
+b %= a        // igual a b = b % a
+console.log(b)
