dedac75a5bc7cffd5f00165c9c29db03e2154c26
Julio Gomes <106027379+Juliogomz@users.noreply.github.com>
2022-08-10 19:26:33 -0300

    JS-Update

---
+++ Tipagem/String/Operações String/Operações String.js
@@ -0,0 +1,9 @@
+const escola = "S0nia"
+
+console.log(escola.charAt(2)) 
+//Pot meio do operador ".charAt()" é possível selecionar um dos caracteres específicos da string.
+
+console.log(escola.substring(1))
+console.log(escola.substring(0, 3))
+//Pot meio do operador ".substring" é possível escolher a partir de qual parte irá imprimir a string.
+
